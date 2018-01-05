9ceeb507a9bc4c58ebc78708ddc84d5d3f0d0190
Julia <yulya-777@mail.ru>
2018-01-05 18:03:05 +0300

    Заполнение анкеты

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,24 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+	const slider = document.querySelector('.js-level');
+	const triangle = document.querySelector('.js-level__triangle');
+	let mouseDown = false;
+
+	triangle.onmousedown = function () {
+		mouseDown = true;
+	};
+
+	document.documentElement.onmouseup = function () {
+		mouseDown = false;
+	};
+
+	slider.onmousemove = function (e) {
+		if (e.target === slider) {
+			const x = e.offsetX - 9; // 9 - половина ширины треугольника
+			if (mouseDown) {
+				triangle.style.left = x + 'px';
+			}
+		}
+	};
 });
